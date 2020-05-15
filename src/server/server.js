46aa45d0ef2ac9100116bf486ccc584e12f25167
Tanja Babic-Debrosse <58664635+tbd007@users.noreply.github.com>
2020-05-15 13:56:48 +0200

    Update server.js

--- src/server/server.js
+++ src/server/server.js
@@ -25,31 +25,6 @@ app.get('/',function(req,res){
   res.sendFile('dist/index.html')
 })
 
-const formtext = document.getElementById('countryCode').value;
-    if (!formtext) return;
-    console.log(formtext);
-        fetch("http://localhost:3000/api", {
-        method: "POST",
-        mode:"cors",
-        headers: {
-        "Accept": "application/json, text/plain, */*",
-        "Content-Type": "application/json"
-        },
-        body: JSON.stringify({formtext})
-    })
-        .then(res => res.json())
-        .then(function(res) {
-        console.log(res);
-        })
-        .then(data => {
-            document.getElementById('date').innerHTML += data.currencies[0].code + data.languages[0].name;
-        })
-
-
-
-
-
-  
 
 
 
