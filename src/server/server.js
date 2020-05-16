04e176bb5928425689dde61447a3de37e95d8799
Tanja Babic-Debrosse <58664635+tbd007@users.noreply.github.com>
2020-05-16 10:31:52 +0200

    Update server.js

--- src/server/server.js
+++ src/server/server.js
@@ -15,36 +15,19 @@ app.use(cors());
 // Initialize the main project folder
 app.use(express.static('dist'));
 // Spin up the server
-const port = 3000;
-const server = app.listen(port,()=>{
-    console.log('server running');
-    console.log(`running on localhost:${port}`)
-});
+const port = 5000;
 
 app.get('/',function(req,res){
   res.sendFile('dist/index.html')
 })
 
-app.post("/api", (req, res) => {
-  code = req.body;
-  let baseURL = "https://restcountries.eu/rest/v2/alpha/"
-  let url = baseURL + code;
-  var options = {
-    'method': 'GET',
-    'url': url,
-    'headers': {
-      'Cookie': '__cfduid=dc280fb5db0d0b16bee35d049343ea9691589536067'
-    }
-  };
-  request(options, function (error, response) { 
-    
-    if (error) throw new Error(error);
-    console.log(response.body);
-    data = response.body
-    console.log(data)
-    res.send(data)
-  });
-});
+app.get('/test', async (req, res) => {
+  res.json({message: 'pass!'})
+})
+
+
+
+module.exports = app
 
 
 
